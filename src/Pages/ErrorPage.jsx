070ab3b079f9c6637f50f87f8a608560a06262eb
SaniaAhmed1 <tasminsania77@gmail.com>
2023-06-11 18:00:12 -0700

    error page done

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 
 
 const ErrorPage = () => {
     return (
-        <div>
+        <div className="w-full h-[100vh] flex flex-col justify-center items-center gap-7">
             
+            <img className="w-full mt-28" src="https://img.freepik.com/free-vector/page-found-with-people-connecting-plug-concept-illustration_114360-1927.jpg?size=626&ext=jpg&ga=GA1.1.632902743.1676570136&semt=sph"  />
+            <Link className="text-primary text-xl text-center  my-10 underline" to={'/'}>Go to Home</Link>
         </div>
     );
 };
